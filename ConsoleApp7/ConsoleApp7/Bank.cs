801480e7fdaa89c180635b936197f6eabd28b5b4
KirillShet <146167926+KirillShet@users.noreply.github.com>
2023-10-10 18:27:11 +0300

    Add files via upload

---
+++ ConsoleApp7/ConsoleApp7/Bank.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    internal class Bank
+    {
+        private int nom_schet;
+        public string fio;
+        public float summa;
+        public float dobav;
+        public float minus;
+        public float perevod;
+        public int nom_schet1;
+        public int nom_schet2;
+        public float summa1;
+        public float summa2;
+        public float bodr;
+        public float bodr1;
+        /*public int */
+        public void otk()
+        {
+            Console.WriteLine("Введите номер счета: ");
+            nom_schet = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите ФИО владельца счета: ");
+            fio = Console.ReadLine();
+            Console.WriteLine("Введите сумму счета: ");
+            summa = float.Parse(Console.ReadLine());
+            perevod = 0;
+            Console.Clear();
+        }
+        public void out_inf()
+        {
+            Console.WriteLine("Номер счета: " + nom_schet);
+            Console.WriteLine("ФИО владельца счета: " + fio);
+            Console.WriteLine("Введите сумму счета: " + summa);
+        }
+        public int nom_chet()
+        {
+            return nom_schet;
+        }
+        public float sum()
+        {
+            return summa;
+        }
+        public void dob() 
+        {
+            Console.WriteLine("Введите количество денег, которое вы хотите положить на счет: ");
+            dobav = float.Parse(Console.ReadLine());
+            summa += dobav;
+            Console.WriteLine("Было добавлено на счет: " + dobav + ", теперь на счета: " + summa);
+        }
+        public void umen()
+        {
+            Console.WriteLine("Введите количество денег, которое вы хотите снять со счет: ");
+            minus = float.Parse(Console.ReadLine());
+            if (minus > summa)
+            {
+                Console.WriteLine("Введенное число больше, чем количество денег на счете.");
+            }
+            else
+            {
+                summa -= minus;
+                Console.WriteLine("Было снято со счет: " + minus + ", теперь на счета: " + summa);
+            }
+        }
+        public void obnul() 
+        {
+            Console.WriteLine("Введется обнуление всего счета...");
+            summa -= summa;
+            Console.WriteLine("Теперь на счету: " + summa);
+        }
+        public void perenos()
+        {
+            Console.WriteLine("С какого счета вы хотите перевести деньги?");
+            nom_schet1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("На какой счет вы хотите перевести деньги?");
+            nom_schet2 = Convert.ToInt32(Console.ReadLine());
+        }
+        public int num_chet1()
+        {
+            return nom_schet1;
+        }
+        public int num_chet2()
+        {
+            return nom_schet2;
+        }
+        public float perev()
+        {
+            Console.WriteLine("Сколько вы хотите перевести?");
+            perevod = float.Parse(Console.ReadLine());
+            return perevod;
+        }
+        public void schet1()
+        {
+            bodr = perevod;
+            summa1 = summa;
+            summa1 = summa1 - bodr;
+            Console.WriteLine("На счете под номером: " + nom_chet() + " осталось всего: " + summa1);
+        }
+        public void schet2()
+        {
+            bodr1 = perevod;
+            summa2 = summa;
+            summa2 += bodr1;
+            Console.WriteLine("На счете под номером: " + nom_chet() + " осталось всего: " + summa2);
+        }
+        public void posle_schet1()
+        {
+            summa = summa1;
+        }
+        public void posle_schet2()
+        {
+            summa = summa2;
+        }
+    }
+}
---
+++ ConsoleApp7/ConsoleApp7/Program2.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    public class Program2
+    {
+        static void Main()
+        {
+            Bank[] bank = new Bank[5];
+            int kol = 0;
+            int osh1 = 0;
+            int var = 0;
+            int x = 0;
+            int a = 0;
+            int b = 0;
+            int osh = 0;
+            while (true)
+            {
+                Console.WriteLine("Добро пожаловать в банк 'Карусель'.");
+                Console.WriteLine("Если вы хотите открыть счет, то введите цифру 1.");
+                Console.WriteLine("Если вы хотите получить информацию конкретного счета, то введите 2.");
+                Console.WriteLine("Если вы хотите пополнить какой-то счет, то введите 3.");
+                Console.WriteLine("Если вы хотите снять деньги с какого-то счета, то введите 4.");
+                Console.WriteLine("Если вы хотите снять все деьнги со счета, то введите 5.");
+                Console.WriteLine("Если вы хотите перевести сумму денег с одного счета на другой, то введите 6.");
+                var = Convert.ToInt32(Console.ReadLine());
+                Console.Clear();
+                if (var == 1)
+                {
+                    bank[0] = new Bank();
+                    bank[0].otk();
+                    for (int i = 1; i < bank.Length; i++) 
+                    {
+                        do
+                        {
+                            osh = 0;
+                            bank[i] = new Bank();
+                            bank[i].otk();
+                            kol++;
+                                if (bank[i - 1].nom_chet() == bank[i].nom_chet())
+                                {
+                                    Console.WriteLine("Такой номер уже есть");
+                                    osh++;
+                                }
+                        } while (osh == 1);
+                    }
+                }
+                else if (var == 2)
+                {
+                    if (kol == 0)
+                    {
+                        Console.WriteLine("На данный момент счетов нет.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Какой счет вы хотите вывести: ");
+                        var = Convert.ToInt32(Console.ReadLine());
+                        for (int i = 0; i < bank.Length; i++)
+                        {
+                            if (var == bank[i].nom_chet())
+                            {
+                                bank[i].out_inf();
+                            }
+                        }
+                    }
+                }
+                else if (var == 3)
+                {
+                    if (kol == 0)
+                    {
+                        Console.WriteLine("На данный момент счетов нет.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Какой счет вы хотите вывести: ");
+                        var = Convert.ToInt32(Console.ReadLine());
+                        for (int i = 0; i < bank.Length; i++)
+                        {
+                            if (var == bank[i].nom_chet())
+                            {
+                                bank[i].dob();
+                            }
+                        }
+                    }
+                }
+                else if (var == 4)
+                {
+                    if (kol == 0)
+                    {
+                        Console.WriteLine("На данный момент счетов нет.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Какой счет вы хотите вывести: ");
+                        var = Convert.ToInt32(Console.ReadLine());
+                        for (int i = 0; i < bank.Length; i++)
+                        {
+                            if (var == bank[i].nom_chet())
+                            {
+                                bank[i].umen();
+                            }
+                        }
+                    }
+                }
+                else if (var == 5)
+                {
+                    if (kol == 0)
+                    {
+                        Console.WriteLine("На данный момент счетов нет.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Какой счет вы хотите вывести: ");
+                        var = Convert.ToInt32(Console.ReadLine());
+                        for (int i = 0; i < bank.Length; i++)
+                        {
+                            if (var == bank[i].nom_chet())
+                            {
+                                bank[i].obnul();
+                            }
+                        }
+                    }
+                }
+                else if (var == 6)
+                {
+                    if (kol == 0)
+                    {
+                        Console.WriteLine("На данный момент счетов нет.");
+                    }
+                    else if (kol == 1)
+                    {
+                        Console.WriteLine("Недостаточно счетов");
+                    }
+                    else
+                    {
+                        do
+                        {
+                            osh = 0;
+                            bank[0].perenos();
+                            for (int c = 0; c < bank.Length; c++)
+                            {
+                                if (bank[c].nom_chet() == bank[0].num_chet2())
+                                {
+                                    b = c;
+                                }
+                                else
+                                {
+                                    osh++;
+                                }
+                                
+                            }
+                            if (osh == 4)
+                            {
+                                osh = 0;
+                            }
+                            else
+                            {
+                                Console.WriteLine("Вы ввели неправильное число.");
+                                osh++;
+                            }
+                            for (int i = 0; i < bank.Length; i++)
+                            {
+                                if (bank[i].nom_chet() == bank[0].num_chet1())
+                                {
+                                    a = i;
+                                }
+                                else
+                                {
+                                    osh++;
+                                }
+                            }
+                            if (osh == 4)
+                            {
+                                osh = 0;
+                            }
+                            else
+                            {
+                                Console.WriteLine("Вы ввели неправильный номер счета.");
+                                osh++;
+                            }
+                        } while (osh > 0);
+
+                        if (b == 0)
+                            {
+                                osh++;
+                            }
+                        
+                        osh1 = 0;
+                            do
+                            {
+                                osh1 = 0;
+                                bank[a].perev();
+                                if (bank[a].sum() < bank[a].perev())
+                                {
+                                    Console.WriteLine("На счету нет таких денег.");
+                                    osh1++;
+                                }
+                            } while (osh1 > 0);
+                            bank[b].perev();
+                            bank[a].schet1();                            
+                            bank[a].posle_schet1();
+                            bank[a].out_inf();
+                            bank[b].schet2();
+                            bank[b].posle_schet2();
+                            bank[b].out_inf();
+                    }
+                }
+            }
+        }
+    }
+}
